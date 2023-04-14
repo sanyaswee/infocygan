09cbbfab0975ac83f00a06d90bd68caa495a889f
sanyaswee <kozoriz407@gmail.com>
2023-04-14 16:40:45 +0300

    Create app "registration"

--- infocygan/infocygan/settings.py
+++ infocygan/infocygan/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
--- infocygan/infocygan/urls.py
+++ infocygan/infocygan/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from registration.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
+    path('registration/', registration, name="registration"),
 ]
