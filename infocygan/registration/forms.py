b06cedfd52398c3d6a83f1eda1e380a68560befc
sanyaswee <kozoriz407@gmail.com>
2023-04-22 19:25:16 +0300

    Labels for form

--- infocygan/registration/forms.py
+++ infocygan/registration/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class RegistrationForm(forms.ModelForm):
-    name = forms.CharField(max_length=50)
-    surname = forms.CharField(max_length=50)
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(label='Ім\'я', max_length=50)
+    last_name = forms.CharField(label='Прізвище', max_length=50)
+    username = forms.CharField(label='Нікнейм', min_length=4, max_length=50)
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Підтвердіть пароль', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['name', 'surname', 'username', 'password']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
