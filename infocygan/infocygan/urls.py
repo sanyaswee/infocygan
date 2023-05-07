fdb57ae1392cebebbaa21e29768925b5fec840d3
BronikerBro <nikita.tyagur.phone@gmail.com>
2023-05-07 09:06:15 +0300

    architecture+user_img

--- infocygan/infocygan/urls.py
+++ infocygan/infocygan/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from home.views import *
 from registration.views import *
 from login.views import *
 from add_article.views import add_article
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns = [
     path("add/", add_article, name="add_article"),
     path("forbidden/", forbidden, name="forbidden"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- infocygan/registration/forms.py
+++ infocygan/registration/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserCustom
 
 
 class RegistrationForm(UserCreationForm):
     # first_name = forms.CharField(label='Ім\'я', max_length=50)
     # last_name = forms.CharField(label='Прізвище', max_length=50)
+    logo = forms.ImageField(label='Лого')
     username = forms.CharField(label='Нікнейм', min_length=4, max_length=50)
     email = forms.EmailField(label='Імейл')
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Підтвердіть пароль', widget=forms.PasswordInput)
 
     class Meta:
-        model = User
-        fields = [ 'username', 'email', 'password1', 'password2']
+        model = UserCustom
+        fields = ['logo', 'username', 'email', 'password1', 'password2']
